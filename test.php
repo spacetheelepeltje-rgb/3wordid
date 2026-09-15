2949435d2a6a3cb1a1e6e3e04712ccb65a24c809
Spacetheelepeltje <spacetheelepeltje@gmail.com>
2026-09-15 16:48:31 +0200

    Cleanup unused files 3

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php 
- include 'php/functions.php';
- 
- $db = DB_connect();
-
- DB_insert_3wordid($db,123,'three word id','quite a long text but short in this case','https://www.3wordid.com',1,'frits@rincker.nl');
